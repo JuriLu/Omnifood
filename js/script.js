e508d96fc7e09916eecc8fb39808d84ba64fb75d
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2023-04-03 10:11:32 +0200

    javascript init

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// Set Current Year
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+yearEl.textContent = currentYear
+
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav")
+    ("nav-open")
+
+function checkFlexGap() {
+    var flex = document.createElement("div")
+    flex.style.display = "flex"
+    flex.style.flexDirection = "column"
+    flex.style.rowGap = "1px"
+
+    flex.appendChild(document.createElement("div"))
+    flex.appendChild(document.createElement("div"))
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported)
+
+    if (!isSupported) document.body.classList.add("no-flex-gap")
+}
+
+checkFlexGap();
