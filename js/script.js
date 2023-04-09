0f34cc5bf81076e1841e2f9b5f555cea83b80836
Juri <lubonjajurgen1@gmail.com>
2023-04-09 14:48:31 +0200

    smooth scrolling logic with javascript

--- js/script.js
+++ js/script.js
@@ -11,6 +11,31 @@ btnNav.addEventListener('click', function () {
     headerEl.classList.toggle('nav-open')
 })
 
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link')
+
+allLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const href = link.getAttribute("href")
+        console.log(href)
+
+        // Scroll Back to top
+        if (href === "#") window.scrollTo(
+            {
+                top: 0,
+                behavior: "smooth"
+            }
+        )
+
+        if (href !== "#" && href.startsWith('#')) {
+            const sectionEl = document.querySelector(href)
+            sectionEl.scrollIntoView({behavior: "smooth"})
+        }
+    })
+})
+
 function checkFlexGap() {
     var flex = document.createElement("div")
     flex.style.display = "flex"
